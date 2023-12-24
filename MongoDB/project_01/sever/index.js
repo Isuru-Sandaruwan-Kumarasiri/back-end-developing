69f0e1d0494c36b92f50875a5657fb87918df615
Isuru-Sandaruwan-Kumarasiri <s20426@sci.pdn.ac.lk>
2023-12-25 01:16:07 +0530

    conect database

--- MongoDB/project_01/sever/index.js
+++ MongoDB/project_01/sever/index.js
@@ -1,4 +1,30 @@
-const express=require("express")
-const bodyParser=require("body-parser")
-const mongooes=require("mongoose")
-const cors=require("cors")
+const express=require("express");
+const bodyParser=require("body-parser");
+const mongoose=require("mongoose");
+const cors=require("cors");
+
+
+//create express app
+const app=express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect("mongodb+srv://isurusandaruwan0601:isuru789@cluster0.vklna5u.mongodb.net/mydatabase?retryWrites=true&w=majority",
+{useNewUrlParser:true,
+useUnifiedTopology:true,
+});//to connect database,its give eassy way
+
+
+const connection =mongoose.connection
+connection.once("open",()=>{
+         console.log('MongoDB connected')
+});
+
+//create port 
+const port=5000;
+//create sever
+app.listen(port,()=>{
+    console.log("sever is up and running on part"+port)
+
+})
