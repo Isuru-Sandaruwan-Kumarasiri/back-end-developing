0b50fecf1dd313286e55e2efb9f43c57c01633a7
Isuru-Sandaruwan-Kumarasiri <s20426@sci.pdn.ac.lk>
2023-12-24 22:23:33 +0530

    started sever and run response

---
+++ coreModule/http.js
@@ -0,0 +1,9 @@
+console.log("*******Create a Sever******");
+
+var http=require("http");
+http.createServer(function(request,responce){//call back function
+    // responce.write("MIHIYA")
+    // responce.end()
+            //responce.end("MIHIYA")
+            responce.end("<h1>MIHIYA</h1>")   //get the html output
+}).listen(3000)
